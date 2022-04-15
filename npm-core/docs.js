257a6382c8faa24475f40ecf3d14d92eaf503a7a
KT_NG3 <84909754+KTNG-3@users.noreply.github.com>
2022-04-15 14:57:29 +0700

    commit

---
+++ npm-core/docs.js
@@ -0,0 +1,22 @@
+//include html
+function includeHTML(elementId = 'changeme', html = './pages/Introduced.html') {
+    let thatElement = document.getElementById(elementId);
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                thatElement.innerHTML = this.responseText;
+            }
+
+            if (this.status == 404) {
+                return includeHTML(elementId, '../404.html');
+            }
+        }
+    }
+
+    xhttp.open("GET", html, false);
+    xhttp.send();
+}
+
+includeHTML('changeme', './pages/Introduced.html');
